6846bd6dbbaea8029eb28100c9eaa5f99b13dfc7
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-03-20 11:36:43 +0600

    first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Watch></Watch>
     </div>
   );
+}
+
+function Watch(){
+  const [state, setState] = useState(0)
+  const increaseState =() => setState(state + 1)
+  return(
+    <div style={{border: '2px solid purple',  margin:'20px'}}>
+      <h2>Tis is my watch</h2>
+        <h5>starts:{state} </h5>
+        <button onClick={increaseState}>increase</button>
+        <Display name="Starting" state={state}></Display>
+    </div>
+  )
+}
+
+function Display(props){
+return(
+  <div style={{border:'2px solid blue', margin:'30px'}}>
+    <h2>name: {props.name}</h2>
+    <h5> state: {props.state}</h5>
+  </div>
+)
 }
 
 export default App;
